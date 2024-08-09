49376bc19337d5270d2b9cf09e754e02cf78b0f2
sanchay <97905515+Sanchaykhatwani@users.noreply.github.com>
2024-08-09 14:59:40 +0530

    modified:   Placement_2024/Projects/Contact_us/Contact_us.js

---
+++ Placement_2024/Projects/Contact_us/Contact_us.js
@@ -0,0 +1,11 @@
+function data(){
+
+   var a=document.getElementById("name").value;
+   var b=document.getElementById("email").value;
+   var c=document.getElementById("phone").value;
+
+  if(a=="" || b=="" ||  c==""){
+    alert("Fill all the fileds");
+  }
+
+}
