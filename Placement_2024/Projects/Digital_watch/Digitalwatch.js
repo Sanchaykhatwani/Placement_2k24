2e730124e87c7c31d5b1365bbdf7eaeea3011475
sanchay <97905515+Sanchaykhatwani@users.noreply.github.com>
2024-08-09 15:02:48 +0530

    modified:   Placement_2024/Projects/Contact_us/Contact_us.html 	modified:   Placement_2024/Projects/Contact_us/Contact_us.js

--- Placement_2024/Projects/Digital_watch/Digitalwatch.js
+++
@@ -1,8 +0,0 @@
-let hour = document.getElementById("hour");
-let min = document.getElementById('min');
-let sec = document.getElementById('sec');
-let ampm = document.getElementById("ampm");
-
-let h=new Date().getHours();
-hour.innerHTML=h;
-console(hour);
